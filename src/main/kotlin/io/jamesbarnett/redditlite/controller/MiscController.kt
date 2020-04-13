c2d9c256a5137bec0749cf641d1aa4613f19289d
James Barnett <jkbarnett8@gmail.com>
2020-04-13 16:19:50 +0100

    Add misc endpoints

--- src/main/kotlin/io/jamesbarnett/redditlite/controller/MiscController.kt
+++ src/main/kotlin/io/jamesbarnett/redditlite/controller/MiscController.kt
@@ -2,12 +2,28 @@ package io.jamesbarnett.redditlite.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
 
 @Controller
-class LandingPageController {
+class MiscController {
   @GetMapping("/")
   fun renderLandingPage() : ModelAndView {
     return ModelAndView("landing")
   }
+
+  @GetMapping("/healthcheck")
+  @ResponseBody
+  fun healthCheck() : String {
+    return "ok"
+  }
+
+  @GetMapping("/robots.txt")
+  @ResponseBody
+  fun robotsTxt() : String {
+    return """User-agent: *
+Disallow: /
+"""
+  }
+
 }
